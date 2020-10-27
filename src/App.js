008e7d96940ab764727361fb70592276abbe4013
justin-jones <52481185+justinemanueljones@users.noreply.github.com>
2020-10-27 19:40:25 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios'
 import Card from './components/Card'
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 class App extends React.Component {
   constructor() {
